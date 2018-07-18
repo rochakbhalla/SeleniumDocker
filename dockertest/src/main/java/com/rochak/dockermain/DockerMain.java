3caac5cf6ebecc90a991951d2d01cdf1861cf8ea
Rochak <rbhalla@ETMUM-MB028.local>
2018-07-18 18:52:19 +0530

    first commit

---
+++ dockertest/src/main/java/com/rochak/dockermain/DockerMain.java
@@ -0,0 +1,133 @@
+package com.rochak.dockermain;
+
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.Image;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports.Binding;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.PullImageResultCallback;
+
+//import dockertest.RunningTestOnDocker;
+
+
+public class DockerMain {
+
+	private ExposedPort TCP_4444 = ExposedPort.tcp(4444);
+	private ExposedPort TCP_5900 = ExposedPort.tcp(5900);
+	private int port = 32769;
+	private int debugPort = 32768;
+	private String hostIp = "0.0.0.0";
+	private String containerId;
+	private String container_name = "Container_Chrome_10";
+	private String image_name = "selenium/standalone-chrome";
+	private String debug_image_name = "selenium/standalone-chrome-debug";
+	private String image_tag = "latest";
+	// Here change image_name to debug_image_name in case of debug build running
+	private String image_repo = debug_image_name + ":" + image_tag;
+	private static final DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+
+	public void verifyImage() throws InterruptedException, FileNotFoundException {
+		// Checks if image is present else pulls from repository
+
+		List<Image> images = dockerClient.listImagesCmd().withShowAll(true).exec();
+
+		int ctr = 0;
+		for (int i = 0; i < images.size(); i++) {
+			String[] repos = images.get(i).getRepoTags();
+			if (repos[0].equals(image_repo)) {
+				System.out.println("Image found with tag: " + repos[0]);
+				break;
+			}
+			ctr++;
+		}
+
+		if (ctr == images.size()) {
+			System.out.println("Image not found need to pull form repository");
+			System.out.println("Exceuting commmand --> docker pull " + image_repo);
+			dockerClient.pullImageCmd(image_repo).exec(new PullImageResultCallback()).awaitCompletion(5,
+					TimeUnit.MINUTES);
+
+			String[] lastDownloadedImage = dockerClient.listImagesCmd().withShowAll(true).exec().get(0).getRepoTags();
+
+			if (lastDownloadedImage[0].equals(image_repo)) {
+				System.out.println("Image Dowloaded !!");
+			} else {
+				throw new FileNotFoundException("Image not downloaded within mentioned time check on Docker");
+			}
+
+		}
+
+	}
+
+	public void startContainer() {
+
+		// Bind server port
+		PortBinding server_port = new PortBinding(Binding.bindIpAndPort(hostIp, port), TCP_4444);
+
+		try {
+			// Creates container and returns contianerId
+			containerId = dockerClient.createContainerCmd(image_name).withPortBindings(server_port)
+					.withName(container_name).exec().getId();
+
+			// Start the container
+			dockerClient.startContainerCmd(containerId).exec();
+			System.out.println("Container Started with ID: " + containerId);
+			Thread.sleep(10000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void startContianerWithDebugPorts() {
+
+		PortBinding server_port = new PortBinding(Binding.bindIpAndPort(hostIp, port), TCP_4444);
+		PortBinding debug_port = new PortBinding(Binding.bindIpAndPort(hostIp, debugPort), TCP_5900);
+
+		try {
+			// Creates container and returns cotnianerId
+			containerId = dockerClient.createContainerCmd(debug_image_name).withPortBindings(server_port, debug_port)
+					.withName(container_name + "-Debug").exec().getId();
+
+			// Start the container
+			dockerClient.startContainerCmd(containerId).exec();
+			System.out.println("Container Started with ID: " + containerId);
+			Thread.sleep(10000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void browserTest() throws Exception {
+		
+		
+	}
+
+	public void stopContainer() {
+
+		try {
+
+			dockerClient.stopContainerCmd(containerId).withTimeout(3).exec();
+			InspectContainerResponse inspectContainerResponse = dockerClient.inspectContainerCmd(containerId).exec();
+
+//			Boolean bb = inspectContainerResponse.getState().getRunning();
+//			Boolean mm = false;
+//			assertEquals(bb, mm);
+			System.out.println("Container Stopped with exit code:" + inspectContainerResponse.getState().getExitCode());
+
+			// To remove the container if required.
+			dockerClient.removeContainerCmd(containerId).exec();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ dockertest/src/test/java/com/rochak/dockertest/DockerTests.java
@@ -0,0 +1,69 @@
+package com.rochak.dockertest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+
+import com.rochak.dockermain.DockerMain;
+
+
+public class DockerTests {
+
+	DockerMain etm = new DockerMain();
+	
+	@BeforeTest
+	public void setUp() throws FileNotFoundException, InterruptedException {
+		etm.verifyImage();
+	}
+
+	@Test(priority = 0, enabled = true)
+	public void testStartContianerWithDebugPorts() {
+
+		etm.startContianerWithDebugPorts();
+	}
+
+	@Test(priority = 1)
+	public void testBrowserTest() throws Exception {
+		DesiredCapabilities dcap = DesiredCapabilities.chrome();
+
+        // Check the Port No here.
+        URL selenium_standalone_host = new URL("http://0.0.0.0:32769/wd/hub");
+       
+        WebDriver driver = new RemoteWebDriver(selenium_standalone_host, dcap);
+
+        			System.out.println("****** Test Started ******");
+        		   driver.get("http://www.spicejet.com");
+        		  //driver.manage().window().maximize();
+        		   Select s = new Select(driver.findElement(By.name("ctl00$mainContent$DropDownListCurrency")));
+        		   s.selectByValue("USD");
+        		   s.selectByIndex(2);
+        		   s.selectByVisibleText("USD");
+        		   driver.findElement(By.xpath(("//input[@name='ctl00_mainContent_ddl_originStation1_CTXT']"))).click();
+        		   driver.findElement(By.xpath(("//a[@value='DHM']"))).click();
+        		   Thread.sleep(60);
+        		   driver.findElement(By.xpath("(//a[@value='GOI'])[2]")).click();
+        		   System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+        		   Thread.sleep(1000);
+        		   driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+        		   System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+        		   Thread.sleep(1000);
+        		   driver.findElement(By.id("ctl00_mainContent_chk_StudentDiscount")).click();
+        		   driver.close();
+        		   driver.quit();
+        		   System.out.println("**** This is end of the test ****");
+	}
+
+	
+//	@AfterTest
+//	public void tearDown() {
+//		etm.stopContainer();
+//	}
+}
